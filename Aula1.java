325711052c78be1afe7e2fc15e9faaee8e76882c
Heitor_jh <146966326+4gatorr@users.noreply.github.com>
2026-08-12 13:13:26 -0300

    Change greeting from 'oi' to 'Hello World!'

--- Aula1.java
+++ Aula1.java
@@ -1,7 +1,7 @@
 public class Aula1 {
 
-    public static void main (String []args){
-        System.out.println("oi");
+    public static void main (String [] args){
+        System.out.println("Hello World!");
     }
 
 }
