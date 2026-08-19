7533b43cc22ca52f6254847196d58410a21e7a19
Heitor_jh <146966326+4gatorr@users.noreply.github.com>
2026-08-19 14:38:12 +0000

    SaveAula2

---
+++ Exemplo1.java
@@ -0,0 +1,11 @@
+public class Exemplo1 {
+    public static void main (String [] args){
+        
+        System.out.println("Esse algoritmo irá listar os meus 5 livros favoritos.");
+        System.out.println("Livro 1: Noiva");
+        System.out.println("Livro 2: A paciente silenciosa");
+        System.out.println("Livro 3: Verity");
+        System.out.println("Livro 4: O Massacre da Familia Hope");
+        System.out.println("Livro 5: Ordem Paranormal - OSNF");
+    }  
+}
