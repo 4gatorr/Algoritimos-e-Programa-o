0d1b9856dacbc67cbfca560207f98356a323af44
Heitor_jh <146966326+4gatorr@users.noreply.github.com>
2026-08-19 19:26:23 +0000

    PuleiLinha

--- Exemplo1.java
+++ Exemplo1.java
@@ -1,8 +1,8 @@
 public class Exemplo1 {
     public static void main (String [] args){
         
-        System.out.println("Esse algoritmo irá listar os meus 5 livros favoritos.");
-        System.out.println("Livro 1: Noiva");
+        System.out.println("\nEsse algoritmo irá listar os meus 5 livros favoritos.");
+        System.out.println("\nLivro 1: Noiva");
         System.out.println("Livro 2: A paciente silenciosa");
         System.out.println("Livro 3: Verity");
         System.out.println("Livro 4: O Massacre da Familia Hope");
